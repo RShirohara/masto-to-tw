b9088609cafeca38c424147819cf6c3111db51a6
Ray Shirohara <RShirohara@proton.me>
2024-11-23 21:18:34 +0900

    feat: add process for retrieve mastodon statuses

---
+++ src/api/mastodon.rs
@@ -0,0 +1,134 @@
+use std::error::Error;
+
+use reqwest::{header, Client, Response};
+use serde::{Deserialize, Serialize};
+use worker::Env;
+
+const USER_AGENT: &str = "masto-to-tw";
+
+pub struct Api {
+  auth: Auth,
+  client: Client,
+}
+
+impl Api {
+  pub fn new(env: &Env) -> Result<Self, Box<dyn Error>> {
+    Ok(Self {
+      auth: Auth::from_worker_env(env)?,
+      client: Client::new(),
+    })
+  }
+}
+
+// Authentication
+struct Auth {
+  access_token: String,
+  domain: String,
+}
+
+impl Auth {
+  pub fn from_worker_env(env: &Env) -> Result<Self, Box<dyn Error>> {
+    Ok(Self {
+      access_token: env.secret("MASTODON_ACCESS_TOKEN")?.to_string(),
+      domain: env.secret("MASTODON_INSTANCE_URL")?.to_string(),
+    })
+  }
+}
+
+// Account
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Account {
+  pub id: String,
+}
+
+impl Api {
+  pub async fn lookup_account(&self, acct: &str) -> Result<Account, Box<dyn Error>> {
+    let response = self
+      .client
+      .get(format!(
+        "{domain}/api/v1/accounts/lookup",
+        domain = self.auth.domain,
+      ))
+      .query(&[("acct", acct)])
+      .header(header::USER_AGENT, USER_AGENT)
+      .bearer_auth(self.auth.access_token.as_str())
+      .send()
+      .await?;
+    let account: Account = serde_json::from_str(response.text().await?.as_str())?;
+
+    Ok(account)
+  }
+}
+
+// Status
+#[derive(Clone, Serialize, Deserialize)]
+pub struct Status {
+  pub account: Account,
+  pub id: String,
+  pub in_reply_to_account_id: Option<String>,
+  pub in_reply_to_id: Option<String>,
+  pub media_attachments: Vec<MediaAttachment>,
+  pub spoiler_text: String,
+  pub text: String,
+  pub url: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct MediaAttachment {
+  pub description: Option<String>,
+  pub url: String,
+}
+
+impl Api {
+  pub async fn get_account_status(&self, account: &Account) -> Result<Vec<Status>, Box<dyn Error>> {
+    let response = self
+      .client
+      .get(format!(
+        "{domain}/api/v1/accounts/{account_id}/statuses",
+        domain = self.auth.domain,
+        account_id = account.id
+      ))
+      .query(&[("exclude_reblogs", true), ("only_public", true)])
+      .header(header::USER_AGENT, USER_AGENT)
+      .bearer_auth(self.auth.access_token.as_str())
+      .send()
+      .await?;
+    let statuses: Vec<Status> = serde_json::from_str(response.text().await?.as_str())?;
+
+    Ok(statuses)
+  }
+}
+
+// Media
+pub struct Media {
+  pub content_type: String,
+  pub content_size: u64,
+  pub response: Response,
+}
+
+impl Api {
+  pub async fn get_media_attachment(&self, url: &str) -> Result<Media, Box<dyn Error>> {
+    let response = self
+      .client
+      .get(url)
+      .header(header::USER_AGENT, USER_AGENT)
+      .send()
+      .await?;
+
+    Ok(Media {
+      content_type: response
+        .headers()
+        .get(header::CONTENT_TYPE)
+        .unwrap()
+        .to_str()?
+        .to_string(),
+      content_size: response
+        .headers()
+        .get(header::CONTENT_LENGTH)
+        .unwrap()
+        .to_str()?
+        .parse::<u64>()?,
+      response,
+    })
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,32 @@
+mod api;
+
 use std::error::Error;
 
+use api::mastodon::{Api as MastodonApi, Status};
 use worker::{
   event, Context, Env, Request, Response, Result as WorkerResult, ScheduleContext, ScheduledEvent,
 };
 
 #[event(fetch)]
-pub async fn fetch(_req: Request, env: Env, _ctx: Context) -> WorkerResult<Response> {
-  Response::ok("Hello, workers!")
+pub async fn fetch(_req: Request, env: Env, ctx: Context) -> WorkerResult<Response> {
+  match sync_posts(&env, &ctx).await {
+    Ok(statuses) => Response::ok(serde_json::to_string(&statuses)?),
+    Err(error) => Response::error(error.to_string(), 500),
+  }
 }
 
 #[event(scheduled)]
 pub async fn scheduled(_event: ScheduledEvent, env: Env, _ctx: ScheduleContext) {}
 
-async fn sync_posts(env: &Env) -> Result<(), Box<dyn Error>> {
-  Ok(())
+async fn sync_posts(env: &Env, ctx: &Context) -> Result<Vec<Status>, Box<dyn Error>> {
+  // Get mastodon account
+  let mastodon_api = MastodonApi::new(env)?;
+  let mastodon_account = mastodon_api
+    .lookup_account(env.secret("MASTODON_ACCOUNT_ACCT")?.to_string().as_ref())
+    .await?;
+
+  // Get mastodon statuses
+  let mastodon_statuses = mastodon_api.get_account_status(&mastodon_account).await?;
+
+  Ok(mastodon_statuses)
 }
