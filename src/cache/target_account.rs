bfbf50f2f543cfd2893cc7db414f47ebe284f59d
Ray Shirohara <RShirohara@proton.me>
2024-12-01 16:07:17 +0900

    style: change indent style to 4 spaces on `.rs` file

--- src/cache/target_account.rs
+++ src/cache/target_account.rs
@@ -7,21 +7,21 @@ use crate::api::mastodon::Account;
 use super::{save_to_kv, KV_BINDING_NAME};
 
 pub async fn get_target_account(env: &Env) -> Result<Option<Account>, Box<dyn Error>> {
-  let kv = env.kv(KV_BINDING_NAME)?;
-  let account: Option<Account> = kv.get("target_account").json().await?;
+    let kv = env.kv(KV_BINDING_NAME)?;
+    let account: Option<Account> = kv.get("target_account").json().await?;
 
-  Ok(account)
+    Ok(account)
 }
 
 pub fn save_target_account(
-  env: &Env,
-  ctx: &ScheduleContext,
-  account: &Account,
+    env: &Env,
+    ctx: &ScheduleContext,
+    account: &Account,
 ) -> Result<(), Box<dyn Error>> {
-  let kv = env.kv(KV_BINDING_NAME)?;
-  let account_encoded = serde_json::to_string(account)?;
+    let kv = env.kv(KV_BINDING_NAME)?;
+    let account_encoded = serde_json::to_string(account)?;
 
-  ctx.wait_until(save_to_kv(kv, "target_account", account_encoded, 86400));
+    ctx.wait_until(save_to_kv(kv, "target_account", account_encoded, 86400));
 
-  Ok(())
+    Ok(())
 }
